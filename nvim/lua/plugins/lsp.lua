685e84c378d8f94dbe1ca0def1893ee479a8f384
Rasmus Prip <pripman2@gmail.com>
2024-08-26 22:23:00 +0200

    changed lua snippet config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -65,9 +65,61 @@ return {
 			})
 
 			local cmp = require 'cmp'
+			local luasnip = require("luasnip")
+
+
+
 			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require'luasnip'.lsp_expand(args.body)
+					end
+				},
+
+				sources = cmp.config.sources({
+					{ name = "luasnip" },
+					{ name = "nvim_lsp",               keyword_length = 1 },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "path" },
+					{ name = "nvim_lua" }
+				}, {
+					{ name = 'buffer' },
+				}),
+
 				mapping = cmp.mapping.preset.insert({
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
+					['<CR>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							if luasnip.expandable() then
+								luasnip.expand()
+							else
+								cmp.confirm({
+									select = true,
+								})
+							end
+						else
+							fallback()
+						end
+					end),
+
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				preselect = 'item',
 				completion = {
@@ -81,11 +133,11 @@ return {
 					{ name = 'buffer' },
 				}
 			})
+
 		end,
 		branch = 'v3.x'
 	},
 	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/nvim-cmp' },
-	{ 'L3MON4D3/LuaSnip' }
+	{ 'hrsh7th/nvim-cmp', dependencies={'L3MON4D3/LuaSnip'} },
 }
